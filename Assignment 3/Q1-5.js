af65853966878fa58c7684bd876b3c505f509c27
Zen-XD <chreemaharjan3@gmail.com>
2026-01-10 22:10:49 +0545

    merged Q1-10

--- Assignment 3/Q1-5.js
+++ Assignment 3/Q1-5.js
@@ -1,5 +1,4 @@
 // 1. Age Check
-
 let age = 17;
 
 if (age >= 18) {
@@ -11,7 +10,6 @@ else {
 
 
 // 2. Number Sign
-
 let num1 = 20;
 
 if (num1 >= 0) {
@@ -23,7 +21,6 @@ else {
 
 
 // 3. Even or Odd
-
 let num2 = 21;
 
 if (num2 % 2 == 0) {
@@ -35,10 +32,9 @@ else {
 
 
 // 4. Vowel or Consonant
+let letter1 = 'I';
 
-let letter = 'I';
-
-if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u' || letter == 'A' || letter == 'E' || letter == 'I' || letter == 'O' || letter == 'U') {
+if (letter1 == 'a' || letter1 == 'e' || letter1 == 'i' || letter1 == 'o' || letter1 == 'u' || letter1 == 'A' || letter1 == 'E' || letter1 == 'I' || letter1 == 'O' || letter1 == 'U') {
     console.log("Vowel")
 }
 else {
@@ -47,7 +43,6 @@ else {
 
 
 // 5. Check Zero
-
 let num3 = 0;
 
 if (num3 == 0) {
@@ -55,4 +50,59 @@ if (num3 == 0) {
 }
 else {
     console.log("Non-zero");
+}
+
+// 6. Multiple of Five
+let num4 = 21;
+
+if (num1 % 5 == 0) {
+    console.log("Multiple of 5");
+}
+else {
+    console.log("Not a multiple of 5");
+}
+
+
+// 7. Greater Than Ten
+let num5 = 10;
+
+if (num2 > 10) {
+    console.log("Greater than 10");
+}
+else {
+    console.log("10 or less");
+}
+
+
+// 8. Check Equality
+let num6 = 10;
+let num7 = 12;
+
+if (num3 == num4) {
+    console.log("Equal");
+}
+else {
+    console.log("Not Equal");
+}
+
+
+// 9. String Length Check
+let str = "hello world";
+let length = str.length
+
+if (length > 5) {
+    console.log("Long string");
+}
+else {
+    console.log("Short string");
+}
+
+
+// 10. Check Uppercase
+let letter2 = "A"
+
+if (letter2 >= 'A' && letter2 <= 'Z') {
+    console.log("Uppercase");
+} else {
+    console.log("Lowercase");
 }
