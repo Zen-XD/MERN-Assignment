ac5680915c4703371db96ef42048109acf3f8ba9
Zen-XD <chreemaharjan3@gmail.com>
2026-01-18 12:01:43 +0000

    assignemnt/array

---
+++ Assignment 6/Array_Questions.js
@@ -0,0 +1,186 @@
+let Q1 = () => {
+    let arr = [1, 2, 3, 4];
+    arr.push(7);
+    console.log(arr);
+}
+// Q1();
+
+
+
+let Q2 = () => {
+    let arr = ['apple', 'banana'];
+    arr.pop();
+    console.log(arr);
+}
+// Q2();
+
+
+
+let Q3 = () => {
+    let arr = [10, 20, 30, 40];
+    arr.shift();
+    console.log(arr);
+}
+// Q3();
+
+
+
+let Q4 = () => {
+    let arr = ['middle', 'end'];
+    arr.unshift('start');
+    console.log(arr);
+}
+// Q4();
+
+
+
+let Q5 = () => {
+    let arr = ['one', 'two', 'three'];
+    let str = arr.toString();
+    console.log(str);
+}
+// Q5(5);
+
+
+
+let Q6 = () => {
+    let arr1 = ['cat', 'dog'];
+    let arr2 = ['fish', 'bird'];
+    let arr = arr1.concat(arr2);
+    console.log(arr);
+}
+// Q6();
+
+
+
+let Q7 = () => {
+    let arr = ['red', 'green', 'blue'];
+    let newArr = arr.slice(0, 2);
+    console.log(newArr);
+}
+// Q7();
+
+
+
+let Q8 = () => {
+    let arr = ['x', 'y', 'z'];
+    arr.pop();
+    arr.unshift('a');
+    console.log(arr);
+}
+// Q8();
+
+
+
+let Q9 = () => {
+    let arr = [100, 200, 300];
+    let str = arr.toString();
+    console.log(str.length);
+}
+// Q9();
+
+
+
+let Q10 = () => {
+    let arr = ['apple', 'banana'];
+    arr.push('orange');
+    arr.push('grape');
+    console.log(arr);
+}
+// Q10();
+
+
+
+let Q11 = () => {
+    let arr = ['sun', 'moon', 'stars'];
+    arr.shift();
+    console.log(arr);
+}
+// Q11();
+
+
+
+let Q12 = () => {
+    let arr = [7, 14, 21];
+    arr.unshift(42);
+    console.log(arr);
+}
+// Q12();
+
+
+
+let Q13 = () => {
+    let arr = [5, 10, 15, 20];
+    let num = arr.pop();
+    console.log(num);
+}
+// Q13();
+
+
+
+let Q14 = () => {
+    let arr = ['hello', 'world'];
+    let str = arr.toString();
+    let newArr = str.split(',');
+    console.log(newArr);
+}
+// Q14();
+
+
+
+let Q15 = () => {
+    let arr1 = ['x', 'y'];
+    let arr2 = [1, 2];
+    let arr = arr1.concat(arr2);
+    console.log(arr);
+}
+// Q15();
+
+
+
+let Q16 = () => {
+    let arr = [1, 2, 3, 4];
+    arr.push(99);
+    arr.shift();
+    console.log(arr);
+}
+// Q16();
+
+
+
+let Q17 = () => {
+    let arr = ['a', 'b', 'c', 'd'];
+    newArr = arr.slice(2, 5);
+    console.log(newArr);
+}
+// Q17();
+
+
+
+let Q18 = () => {
+    let arr = ['elephant', 'giraffe'];
+    arr.unshift('lion');
+    arr.unshift('tiger');
+    console.log(arr.length);
+}
+// Q18();
+
+
+
+let Q19 = () => {
+    let arr = [20, 30, 40];
+    let str = arr.toString();
+    let newArr = str.split(',');
+    console.log(newArr);
+}
+// Q19();
+
+
+
+let Q20 = () => {
+    let arr = ['apple', 'banana', 'orange'];
+    arr.push('grape');
+    arr.pop();
+    console.log(arr);
+}
+// Q20();
